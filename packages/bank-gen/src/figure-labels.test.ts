73d3f50f3acb713608c5538353525c92a048bd73
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2026-01-15 21:00:58 +0700

    test: Add unit tests for `replaceFigureReferences` function covering language-specific figure labels and package detection.

--- packages/bank-gen/src/figure-labels.test.ts
+++ packages/bank-gen/src/figure-labels.test.ts
@@ -9,32 +9,32 @@ describe("replaceFigureReferences", () => {
     it("should use 'Figure' for 'en' language", () => {
         const input = "See \\figurename~\\ref{fig:test}";
         const output = replaceFigureReferences(input, labelMap, "en");
-        expect(output).toBe('See <a href="#fig-fig:test" class="latex-ref">Figure 1</a>');
+        expect(output).toBe('See <a href="#fig-fig:test" class="latex-ref">Figure [[FIG_NUM_fig:test]]</a>');
     });
 
     it("should use 'Hình' for 'vi' language", () => {
         const input = "See \\figurename~\\ref{fig:test}";
         const output = replaceFigureReferences(input, labelMap, "vi");
-        expect(output).toBe('See <a href="#fig-fig:test" class="latex-ref">Hình 1</a>');
+        expect(output).toBe('See <a href="#fig-fig:test" class="latex-ref">Hình [[FIG_NUM_fig:test]]</a>');
     });
 
     it("should default to 'vi' if language is not provided", () => {
         const input = "See \\figurename~\\ref{fig:test}";
         const output = replaceFigureReferences(input, labelMap);
-        expect(output).toBe('See <a href="#fig-fig:test" class="latex-ref">Hình 1</a>');
+        expect(output).toBe('See <a href="#fig-fig:test" class="latex-ref">Hình [[FIG_NUM_fig:test]]</a>');
     });
 
     it("should ignore LaTeX package detection (implicit logic removed)", () => {
         // Even with explicit english package or context, if language is 'vi', it should output 'Hình'
         const input = "\\usepackage[english]{babel} See \\figurename~\\ref{fig:test}";
         const output = replaceFigureReferences(input, labelMap, "vi");
-        expect(output).toBe('\\usepackage[english]{babel} See <a href="#fig-fig:test" class="latex-ref">Hình 1</a>');
+        expect(output).toBe('\\usepackage[english]{babel} See <a href="#fig-fig:test" class="latex-ref">Hình [[FIG_NUM_fig:test]]</a>');
     });
 
     it("should ignore LaTeX package detection for english", () => {
         // Even with explicit vietnam package, if language is 'en', it should output 'Figure'
         const input = "\\usepackage{vietnam} See \\figurename~\\ref{fig:test}";
         const output = replaceFigureReferences(input, labelMap, "en");
-        expect(output).toBe('\\usepackage{vietnam} See <a href="#fig-fig:test" class="latex-ref">Figure 1</a>');
+        expect(output).toBe('\\usepackage{vietnam} See <a href="#fig-fig:test" class="latex-ref">Figure [[FIG_NUM_fig:test]]</a>');
     });
 });
